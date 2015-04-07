601aa780ebf9874290b2b0057178707c754105be
jing mao <jingmao88@gmail.com>
2015-04-07 17:24:39 +0000

    initial commit

--- assets_route.py
+++ assets_route.py
@@ -2,20 +2,20 @@ from bottle import route, static_file, get
 
 ################################################################################
 # Static Routes: js, css, img, fonts
-
+root_url = '/home/jmao/mapserver/'
 @get('/<filename:re:.*\.js>')
 def javascripts(filename):
-    return static_file(filename, root='assets/js')
+    return static_file(filename, root= root_url + 'assets/js')
 
 @get('/<filename:re:.*\.css>')
 def stylesheets(filename):
-    return static_file(filename, root='assets/css')
+    return static_file(filename, root= root_url + 'assets/css')
 
 @get('/<filename:re:.*\.(jpg|png|gif|ico)>')
 def images(filename):
-    return static_file(filename, root='assets/img')
+    return static_file(filename, root= root_url + 'assets/img')
 
 @get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
 def fonts(filename):
-    return static_file(filename, root='assets/fonts')
+    return static_file(filename, root= root_url + 'assets/fonts')
 
--- bottle_app.py
+++ bottle_app.py
@@ -1,5 +1,12 @@
 import assets_route
 from bottle import route, run, debug, template, request, static_file, error, get, default_app
-@route('/')
+
+root_url = '/home/jmao/mapserver/'
+@route('/', method='GET')
 def load():
-    return static_file('index.html', root='.')
+    return template(root_url + 'index.tpl')
+
+
+debug(True)
+application = default_app()
+
